ac2365884a2745a990eeefb2de447d11224f61a1
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-05 19:07:52 +0300

    KS-1-1 create smth

--- utils/src/main/kotlin/lesson_1/lesson1_task1.kt
+++ utils/src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -2,7 +2,7 @@ package org.example.utils.lesson_1
 
 fun main(){
     var orders: Int = 75
-    val gratitude: String = "Cпасибо за сделанный заказ!"
+    val gratitude: String = "Cпасибо за заказ!"
     println(orders)
     println(gratitude)
 }
