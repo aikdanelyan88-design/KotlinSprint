966998d48de8f6cae2a6d21f28cc2ba6b2b750c1
ajkdanelan <aik.danelyan88@mail.ru>
2025-10-06 14:19:17 +0300

    KT-1-w3 create smth

---
+++ utils/src/main/kotlin/Sprint_Lesson/lesson1_3work.kt
@@ -0,0 +1,14 @@
+package org.example.utils.Sprint_Lesson
+
+fun main(){
+    val year: Int = 1961
+    var hour: String
+    hour = "09"
+    var minute: String
+    minute = "07"
+    println(year)
+    println("$hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
+}
